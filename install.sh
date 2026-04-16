e7a231c9794d24d871cf5b0b16fa3e9fb057a8fd
Aimer Neige <aimer@linux.com>
2026-04-16 16:46:50 +0000

    change link to github

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://gitlab.aiursoft.com/aiursoft/aiurscript/-/raw/master/$1.sh | sudo bash -s $arg; }
+aiur() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/aiursoftweb/aiurscript/raw/master/$1.sh | sudo bash -s $arg; }
 
 app_name="polls"
-repo_path="https://gitlab.aiursoft.com/aiursoft/polls"
+repo_path="https://github.com/aiursoftweb/polls"
 proj_path="src/Aiursoft.Polls/Aiursoft.Polls.csproj"
 
 get_dll_name()

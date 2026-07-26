1b039c85bcdae9fa0e47331f024165414209519f
Anduin Xue <anduin@aiursoft.com>
2026-07-26 17:57:14 +0000

    Migrate to Aiursoft.UiStack 10.0.22 - embedded static assets

--- src/Aiursoft.Polls/Startup.cs
+++ src/Aiursoft.Polls/Startup.cs
@@ -7,6 +7,7 @@ using Aiursoft.Polls.InMemory;
 using Aiursoft.Polls.MySql;
 using Aiursoft.Polls.Services.Authentication;
 using Aiursoft.Polls.Sqlite;
+using Aiursoft.UiStack;
 using Aiursoft.UiStack.Layout;
 using Aiursoft.UiStack.Navigation;
 using Microsoft.AspNetCore.Mvc.Razor;
@@ -78,6 +79,7 @@ public class Startup : IWebStartup
     {
         app.UseExceptionHandler("/Error/Code500");
         app.UseStatusCodePagesWithReExecute("/Error/Code{0}");
+        app.UseUIStack();
         app.UseStaticFiles();
         app.UseRouting();
         app.UseAuthentication();
